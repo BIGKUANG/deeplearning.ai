20673caad5c23945d8a397b29301cdd68a7c9095
DekeyShen <915320555@qq.com>
2018-01-04 11:03:32 +0800

    complete course work1.2

--- course2/assignment2-1/reg_utils.py
+++ course2/assignment2-1/reg_utils.py
@@ -330,7 +330,6 @@ def load_2D_dataset():
     train_Y = data['y'].T
     test_X = data['Xval'].T
     test_Y = data['yval'].T
-
-    plt.scatter(train_X[0, :], train_X[1, :], c=train_Y, s=40, cmap=plt.cm.Spectral);
-    
+    plt.scatter(train_X[0, :], train_X[1, :], c=train_Y, s=40, cmap=plt.cm.Spectral)
+	
     return train_X, train_Y, test_X, test_Y
